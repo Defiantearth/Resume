24665e68ba57e06553e0567efbcf59444bd50677
Gavin Acosta <155584291+Defiantearth@users.noreply.github.com>
2024-06-05 15:07:01 -0500

    Add files via upload

---
+++ Add Poly/AddTerm.java
@@ -0,0 +1,19 @@
+public class AddTerm{
+   
+   private int coef;
+   private int exp;
+    
+  public AddTerm(int c, int e)
+  { 
+    coef = c;
+    exp = e;    
+  }
+
+  public int getCoef(){
+    return coef;
+  } 
+  
+  public int getExp(){
+    return exp;
+}
+}
